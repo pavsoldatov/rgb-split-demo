d4e2331f9171c07be2445dbb80b0182aa10f0efc
pavsoldatov <snowschoe@gmail.com>
2024-05-10 13:16:09 +0300

    move images

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import glsl from 'vite-plugin-glsl'
 import { resolve } from 'pathe'
 
 export default defineConfig({
+  publicDir: 'public',
   resolve: {
     alias: {
       '/@': resolve(__dirname, './src'),
